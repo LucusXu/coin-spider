5f7bfe548ebc8e6383a2ea8e5392e65deb3691f5
Lucus Xu <liqiuxul5d@gmail.com>
2018-05-14 16:40:07 +0800

    modify

--- env.js
+++ env.js
@@ -3,10 +3,10 @@ module.exports = {
     'debug' : true,
     'logs_root' : '/home/work/logs/node',
     'database' : {
-        'database' : 'wuya',
+        'database' : 'spider',
         'host' : '127.0.0.1',
         'port' : 3306,
         'user' : 'root',
-        'password': 'dI2G1CEM3GTC6nmA',
+        'password': 'root',
     }
 }
